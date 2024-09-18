dfb00345ac92c16ba16509efde8943f0ea230d14
Phillip Compeau <phcompeau@users.noreply.github.com>
2024-09-18 15:13:56 -0400

    Posting final gravity code

--- Finished Code/gravity/drawing.go
+++ Finished Code/gravity/drawing.go
@@ -5,47 +5,100 @@ import (
 	"image"
 )
 
-// let's place our drawing functions here.
+const (
+	trailFrequency        = 10
+	numberOfTrailFrames   = 100
+	jupiterMoonMultiplier = 10.0
+	trailThicknessFactor  = 0.2
+)
 
-// AnimateSystem takes a slice of Universe objects along with a canvas width and an integer drawingFrequency.
-// It generates a slice of images corresponding to drawing each Universe that is divisible by drawingFrequency on a canvasWidth x canvasWidth canvas (in pixels)
 func AnimateSystem(timePoints []Universe, canvasWidth, drawingFrequency int) []image.Image {
 	images := make([]image.Image, 0)
+	trails := make(map[int][]OrderedPair) // Map from body index to its trail of positions
 
 	for i, u := range timePoints {
-		//only draw the current universe to an image IF i is divisible by drawingFrequency
+		// Only draw the frame if the index is divisible by the drawing frequency
+		if (i*trailFrequency)%drawingFrequency == 0 {
+			// Update trails for all bodies
+			for bodyIndex, body := range u.bodies {
+				trails[bodyIndex] = append(trails[bodyIndex], body.position)
+
+				// shorten the current trail if it has exceeded the trail capacity
+				if len(trails[bodyIndex]) > numberOfTrailFrames*trailFrequency {
+					trails[bodyIndex] = trails[bodyIndex][1:] // Limit trail length to MaxTrailLengthFactor
+				}
+			}
+		}
 		if i%drawingFrequency == 0 {
-			images = append(images, DrawToCanvas(u, canvasWidth))
+			images = append(images, DrawToCanvas(u, canvasWidth, trails, i))
 		}
 	}
 
 	return images
 }
 
-// DrawToCanvas generates the image corresponding to a canvas after drawing a Universe
-// object's bodies on a square canvas that is canvasWidth pixels x canvasWidth pixels
-func DrawToCanvas(u Universe, canvasWidth int) image.Image {
-	// set a new square canvas
+func DrawToCanvas(u Universe, canvasWidth int, trails map[int][]OrderedPair, frameCounter int) image.Image {
 	c := canvas.CreateNewCanvas(canvasWidth, canvasWidth)
 
-	// create a white background
+	// set canvas to white
 	c.SetFillColor(canvas.MakeColor(255, 255, 255))
 	c.ClearRect(0, 0, canvasWidth, canvasWidth)
-	c.Fill()
 
-	// range over all the bodies and draw them.
+	// Draw trails for all bodies
+	DrawTrails(&c, trails, frameCounter, u.width, float64(canvasWidth), u.bodies)
+
+	// Draw the bodies themselves
 	for _, b := range u.bodies {
 		c.SetFillColor(canvas.MakeColor(b.red, b.green, b.blue))
-		cx := (b.position.x / u.width) * float64(canvasWidth)
-		cy := (b.position.y / u.width) * float64(canvasWidth)
+		centerX := (b.position.x / u.width) * float64(canvasWidth)
+		centerY := (b.position.y / u.width) * float64(canvasWidth)
 		r := (b.radius / u.width) * float64(canvasWidth)
-		if b.name == "Europa" || b.name == "Io" || b.name == "Ganymede" || b.name == "Callisto" {
-			c.Circle(cx, cy, 10.0*r)
+
+		if b.name == "Io" || b.name == "Ganymede" || b.name == "Callisto" || b.name == "Europa" {
+			c.Circle(centerX, centerY, jupiterMoonMultiplier*r)
 		} else {
-			c.Circle(cx, cy, r)
+			c.Circle(centerX, centerY, r)
 		}
 		c.Fill()
 	}
-	// we want to return an image!
+
 	return c.GetImage()
+}
+
+func DrawTrails(c *canvas.Canvas, trails map[int][]OrderedPair, frameCounter int, uWidth, canvasWidth float64, bodies []Body) {
+	for bodyIndex, b := range bodies {
+		trail := trails[bodyIndex]
+		numTrails := len(trail)
+
+		// Adjust line width based on the body's radius
+		lineWidth := (b.radius / uWidth) * float64(canvasWidth) * trailThicknessFactor // Adjust multiplier for desired thickness
+
+		// adjust lineWidth if we are a jupiter moon
+		if b.name == "Ganymede" || b.name == "Io" || b.name == "Callisto" || b.name == "Europa" {
+			lineWidth *= jupiterMoonMultiplier
+		}
+
+		c.SetLineWidth(lineWidth)
+
+		// Draw lines between consecutive trail points
+		for j := 0; j < numTrails-1; j++ {
+			// Calculate fading effect based on the position in the slice
+			alpha := 255.0 * float64(j) / float64(numTrails)
+			red := uint8((1-alpha/255.0)*255.0 + (alpha/255.0)*float64(b.red))
+			green := uint8((1-alpha/255.0)*255 + (alpha/255.0)*float64(b.green))
+			blue := uint8((1-alpha/255.0)*255 + (alpha/255.0)*float64(b.blue))
+
+			c.SetStrokeColor(canvas.MakeColor(red, green, blue))
+
+			// Draw a line between consecutive trail points
+			startX := (trail[j].x / uWidth) * canvasWidth
+			startY := (trail[j].y / uWidth) * canvasWidth
+			endX := (trail[j+1].x / uWidth) * canvasWidth
+			endY := (trail[j+1].y / uWidth) * canvasWidth
+
+			c.MoveTo(startX, startY)
+			c.LineTo(endX, endY)
+			c.Stroke()
+		}
+	}
 }
